a4ea5a2a6e7dc776fc30073cc9a4188ff2ed4a5c
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-18 17:53:04 +0600

    try catch section added

--- index.js
+++ index.js
@@ -67,6 +67,7 @@ async function run() {
                 sortOption.productCreationDate = -1;
             }
 
+            // try catch
             try {
                 const products = await productCollection
                     .find(query)
