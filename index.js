d5b415e80f581b7e6f74a0d80934f6440add60e0
Osmangonibinismail <mdosmangoni4314@gmail.com>
2024-08-18 17:47:04 +0600

    admin page added

--- index.js
+++ index.js
@@ -76,11 +76,10 @@ const client = new MongoClient(uri, {
     
 
 
-      // Send a ping to confirm a successful connection
+      // Send a ping to confirm a successful connection.
       await client.db("admin").command({ ping: 1 });
       console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
-      // Ensures that the client will close when you finish/error
     //   await client.close();
     }
   }
